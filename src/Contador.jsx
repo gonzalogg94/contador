58eb1b502d43abb53c640e224c5bb0f878f017b1
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2023-02-18 12:55:56 -0300

    agregue cambios en el maquetado

--- src/Contador.jsx
+++ src/Contador.jsx
@@ -2,10 +2,13 @@ import { Container } from "react-bootstrap";
 
 const Contador = () => {
   return (
-    <Container className="pt-4">
+    <Container className="mt-4">
       <section className="text-center">
         <h1>Esto es un proyecto contador</h1>
         <hr></hr>
+        <div className="m-5">
+          20
+        </div>
       </section>
     </Container>
   );
